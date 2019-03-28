b61c55b49cca6e46baae8ac9db0d412bb1820f19
Uday-Vulchi <48994760+Uday-Vulchi@users.noreply.github.com>
2019-03-28 12:54:51 +0530

    Update GCD.py

--- GCD.py
+++ GCD.py
@@ -1,16 +1,3 @@
-#def computeGCD(x, y):
- #   if x > y:
-  #      s = y
-   # else:
-    #    s = x
-    #for i in range(1, s+1):
-     #   if((x % i == 0) and (y % i == 0)):
-         #   GCD = i        
-    #return GCD
-#num1 = int(input())
-#num2 = int(input())
-#print("The G.C.D. of", num1,"and", num2,"is", computeGCD(num1, num2))
-
 def gcd(a,b): 
     if(b==0): 
         return a 
